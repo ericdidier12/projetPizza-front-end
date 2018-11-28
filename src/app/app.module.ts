f96721cc7948882e9d5f8392dbcea164f3143728
eric.nyandwi <nyandwi12>
2018-11-28 06:50:38 +0100

    definitiondeCompossantSequeletteBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Routes, RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Component } from './.component';
+import { FourOhFourComponent } from './FourOhFour/FourOhFour.component';
+import { FourOhFour_Component } from './FourOhFour_/FourOhFour_.component';
+
+const appRoutes : Routes = [
+    { path: 'home', component: WelcomeComponent },
+    { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      WelcomeComponent,
+      FourOhFourComponent,
+      Component,
+      FourOhFour_Component
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
