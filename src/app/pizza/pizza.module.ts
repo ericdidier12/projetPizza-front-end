dc8069aa0619be48e20aee056d7d5ce2f7a1326e
Abdellah <elachhab.abdellah@gmail.com>
2018-12-05 15:10:26 +0100

    shopping kard

--- src/app/pizza/pizza.module.ts
+++ src/app/pizza/pizza.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 
 const appRoutes : Routes = [
-  { path: 'pizzas', component: PizzaListComponent},
+  { path: 'pizzas', component: PizzaListComponent}, 
+  { path: 'shoppingCart', component: ShoppingCartComponent},
+
   ];
 
 @NgModule({
   declarations: [
-    PizzaListComponent
+    PizzaListComponent,
+    ShoppingCartComponent
   ],
   imports: [
     RouterModule.forChild(appRoutes),
